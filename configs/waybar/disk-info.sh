7142ca3446a08b4130617ffa570459c85b8e18e3
n3ssli <62791872+n3ssli@users.noreply.github.com>
2025-03-24 22:33:14 +0000

    Updated configuration

--- configs/waybar/disk-info.sh
+++ configs/waybar/disk-info.sh
@@ -1,7 +1,5 @@
-# Get all mounted filesystems
 MOUNTED_FS=$(df -h | grep '^/dev/' | awk '{print $6 ": " $3 "/" $2 " (" $5 ")"}')
 
-# Format output
 echo "Root: $(df -h / | awk 'NR==2 {print $3 "/" $2 " (" $5 ")"}')"
 echo ""
 echo "All Drives:"
